2a4cc397adf94ca85e191dd9a07e76c71874fcd1
nisit.jiraruwat <nisit.jiraruwat@gmail.com>
2022-02-06 01:09:15 +0700

    Creator animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 import Layout from '@/components/common/Layout'
 
@@ -6,9 +7,19 @@ export default function Home (): JSX.Element {
   return (
     <Layout className='h-full bg-primary-500'>
       <div className='absolute top-24 left-24'>
-        <div className='flex justify-center items-center w-14 h-14 bg-white rounded-full'>
-          <i className='text-secondary-500 fas fa-file fa-2x' />
-        </div>
+        <Link href='/nisit.jiraruwat'>
+          <a className='group block relative w-14 h-14 bg-white rounded-full hover:drop-shadow-xl'>
+            <div className='flex absolute inset-0 justify-center items-center'>
+              <i className='text-secondary-500 group-hover:opacity-0 transition-all duration-300 group-hover:-translate-y-8 fas fa-file fa-2x' />
+            </div>
+            <div className='flex absolute inset-0 justify-center items-center'>
+              <i className='text-secondary-500 opacity-0 group-hover:opacity-100 transition-all duration-300 translate-y-8 group-hover:translate-y-0 fas fa-user-astronaut fa-2x' />
+            </div>
+            <div className='hidden group-hover:block absolute top-full left-1/2 -translate-x-1/2'>
+              <h3 className='flex px-2 pb-1 text-sm text-white bg-zinc-900 rounded'>Creator</h3>
+            </div>
+          </a>
+        </Link>
       </div>
       <div className='flex px-24 pt-28 w-full'>
         <div className='flex flex-none items-center w-3/5'>
